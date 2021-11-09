5d8a9e78576801f2bd3c10e318397113921aa233
H . G <gh.hamid89@gmail.com>
2021-11-09 15:26:48 +0100

    footer style

--- pwa/src/components/common/footer.tsx
+++ pwa/src/components/common/footer.tsx
@@ -2,60 +2,80 @@ import makeStyles from '@mui/styles/makeStyles';
 import * as React from "react";
 import {ThemeSwitcher} from "../theme-switcher/theme-switcher";
 import {Grid} from "@mui/material";
-
-
-
+import CallIcon from '@mui/icons-material/Call';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 const useStyles = makeStyles((theme) => ({
   footerStyle: {
-    marginTop: 50
+    margin: '100px', padding: '0',
+  },
+  text: {
+    backgroundColor: '#3A3A3A',
+    color: '#FFFFFFFF',
+    paddingBottom: '20px',
   },
 }));
 
 export default function Footer() {
+
+  const classes = useStyles();
   const style = {
-    backgroundColor: '#CBCE1C',
+    backgroundColor: '#3A3A3A',
     marginTop: '50px',
-    minHeight: '150px'
+    minHeight: '150px',
   }
 
   return (
-    <footer className="utrecht-page-footer">
-      <Grid container>
-        <Grid item xs={8}>
-          <address className="utrecht-page-footer__address utrecht-page-footer__address--reset-address">
-            <section>
-              <h5 className="utrecht-heading-3 utrecht-heading-3--distanced">Diensten</h5>
+    <footer className="utrecht-page-footer" style={{backgroundColor: '#212121', padding: '0',}}>
+      <div className={classes.text}>
+        <Grid container>
+          <address className="utrecht-page-footer__address utrecht-page-footer__address--reset-address"style={{display: 'flex',}}>
+            <Grid item xs={10}>
+              <div className={classes.footerStyle}>
+                <h5 className="utrecht-heading-3 utrecht-heading-3--distanced"
+                    style={{color: '#FFFFFFFF',}}>Diensten</h5>
+                <p className="utrecht-paragraph utrecht-paragraph--distanced">
+                  <a href="https://nijmegen.commonground.nu/moving"
+                     className="utrecht-link utrecht-link">Verhuizen</a><br/>
+                  <a href="https://nijmegen.commonground.nu/marriage"
+                     className="utrecht-link utrecht-link">Huwelijk</a><br/>
+                  <a href="https://nijmegen.commonground.nu/certificates"
+                     className="utrecht-link utrecht-link">Uittreksels</a>
+                </p>
+              </div>
+            </Grid>
+            <Grid item xs={10}>
+              <div className={classes.footerStyle}>
+                <h5 className="utrecht-heading-3 utrecht-heading-3--distanced"
+                    style={{color: '#FFFFFFFF',}}>Contact</h5>
+                <p className="utrecht-paragraph utrecht-paragraph--distanced">
+                   <LocationOnIcon/><div>Stadswinkel, Mariënburg 30</div><br/>
+                  <CallIcon/> <a href="tel:14024" className="utrecht-link utrecht-link--telephone">14024</a><br/>
+                 <MailOutlineIcon/> <a href="mailto:gemeente@nijmegen.nl"className="utrecht-link utrecht-link--telephone">gemeente@nijmegen.nl</a>
+                </p>
+              </div>
+            </Grid>
+            <Grid item xs={10}>
+            <div className={classes.footerStyle}>
+              <h5 className="utrecht-heading-3 utrecht-heading-3--distanced"
+                  style={{color: '#FFFFFFFF',}}>Openingstijden</h5>
               <p className="utrecht-paragraph utrecht-paragraph--distanced">
-                <a href="https://nijmegen.commonground.nu/moving" className="utrecht-link utrecht-link">Verhuizen</a><br />
-                <a href="https://nijmegen.commonground.nu/marriage" className="utrecht-link utrecht-link">Huwelijk</a><br />
-                <a href="https://nijmegen.commonground.nu/certificates"className="utrecht-link utrecht-link">Uittreksels</a>
-              </p>
-            </section>
-            <section>
-              <h5 className="utrecht-heading-3 utrecht-heading-3--distanced">Contact</h5>
-              <p className="utrecht-paragraph utrecht-paragraph--distanced">
-                <strong>Stadswinkel, Mariënburg 30</strong><br />
-                <a href="tel:14024" className="utrecht-link utrecht-link--telephone">14024</a><br />
-                <a href="mailto:gemeente@nijmegen.nl"
-                   className="utrecht-link utrecht-link--telephone">gemeente@nijmegen.nl</a>
-              </p>
-            </section>
-            <section>
-              <h5 className="utrecht-heading-3 utrecht-heading-3--distanced">Openingstijden</h5>
-              <p className="utrecht-paragraph utrecht-paragraph--distanced">
-                <strong>Maandag - woensdag: 9.00 - 17.00</strong><br />
-                <strong>Donderdag: 9.00 - 20.00</strong><br />
+                <strong>Maandag - woensdag: 9.00 - 17.00</strong><br/>
+                <strong>Donderdag: 9.00 - 20.00</strong><br/>
                 <strong>Vrijdag: 9.00 - 17.00</strong>
               </p>
-            </section>
+            </div>
+          </Grid>
           </address>
-        </Grid>
-        <Grid item xs={4}>
-          <div style={{marginTop: 'auto', marginLeft: 'auto', width: "fit-content"}}>
+         <Grid item xs={6} >
+          <div style={{marginTop: 'auto',  marginLeft: 'auto', width: "fit-content"}}>
             <ThemeSwitcher/>
           </div>
-        </Grid>
+         </Grid>
       </Grid>
-    </footer>
-  );
+    </div>
+</footer>
+)
+  ;
 }
+
