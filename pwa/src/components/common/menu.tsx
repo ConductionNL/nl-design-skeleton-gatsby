23940e6c3edf471c0807c9647376f80427d210a7
ginokok1996 <ginokok1996@hotmail.com>
2021-11-10 11:51:31 +0100

    fixes

--- pwa/src/components/common/menu.tsx
+++ pwa/src/components/common/menu.tsx
@@ -4,9 +4,9 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import {Stack} from "@mui/material";
 import * as React from "react";
-import {useRouter} from 'next/router';
-import {useAppContext} from "../context/state";
-import {useUserContext} from "../context/userContext";
+import {useEffect} from "react";
+import {useUserContext} from "../../context/userContext";
+import {useUrlContext} from "../../context/urlContext";
 const useStyles = makeStyles((theme) => ({
   grow: {
     flexGrow: 1,
@@ -30,60 +30,20 @@ const handleLogout = (context) => {
 }
 
 export default function MainMenu() {
-  let userContext = useUserContext();
 
   const classes = useStyles();
-  const router = useRouter()
-
-
-  const [state, setState] = React.useState({
-    displayMenuDrawer: false,
-    displayUserDrawer: false,
-    loggedIn: false,
-  });
-
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
-
-    setState({...state, [anchor]: open});
-  };
-
-  const loginUser = (status) => {
-    setState({...state, ['loggedIn']: status});
-  };
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      if (sessionStorage.getItem('user') !== null) {
-        userContext.setUser(JSON.parse(sessionStorage.getItem('user')));
-      }
-    }
-  }, []);
-
-
 
   const handleLogout = () => {
     sessionStorage.setItem('user', null);
-    userContext.setUser(null);
-    router.push('/');
+
   }
 
   let userContext = useUserContext();
-  let context = useAppContext();
+  let context = useUrlContext();
+
+  console.log(context);
 
   return (
-    <div className="utrecht-navhtml">
-      <nav className="topnav">
-        <ul className="utrecht-topnav__list">
-          <li className="utrecht-topnav__item">
-            {
-              userContext.user !== undefined && userContext.user !== null ?
-                <span>{userContext.user.name}</span> :
-                <a className="utrecht-topnav__link"
-                  href="/"
-                  target="_blank">Inloggen</a>
 
     <div className="utrecht-navhtml" >
       <nav className="topnav"   >
