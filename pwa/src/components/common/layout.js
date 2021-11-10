c0f3374c34bd18226253c8986114d1bd32ee16a7
ginokok1996 <ginokok1996@hotmail.com>
2021-11-10 10:46:43 +0100

    nijmegne default

--- pwa/src/components/common/layout.js
+++ pwa/src/components/common/layout.js
@@ -24,9 +24,9 @@ export default function Layout({ children }) {
         <>
             <ThemeProvider theme={theme}>
               <Helmet>
-                <link rel="stylesheet" href="https://unpkg.com/@conductionnl/demodam-design-tokens/src/index.css"/>
+                <link rel="stylesheet" href="https://unpkg.com/@conductionnl/nijmegen-design-tokens/dist/index.css"/>
               </Helmet>
-              <div className="utrecht-document demodam-theme">
+              <div className="utrecht-document nijmegen-theme">
                 <div className="utrecht-page">
                   <Header/>
                   <MainMenu />
